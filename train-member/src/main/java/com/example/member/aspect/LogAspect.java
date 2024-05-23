2184c04e9210b356617760e583f7e3cb7d6e9879
eddie-code <231071372@qq.com>
2024-05-23 16:54:45 +0800

    4.7 增加LogAspect, 打印请求参数和返回结果, 以及日志流水号

---
+++ train-member/src/main/java/com/example/member/aspect/LogAspect.java
@@ -0,0 +1,102 @@
+package com.example.member.aspect;
+
+import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author lee
+ * @description
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
+    public LogAspect() {
+//        System.out.println("Common LogAspect");
+        LOG.info("Common LogAspect");
+    }
+
+    /**
+     * 定义一个切点
+     */
+    @Pointcut("execution(public * com.example..*Controller.*(..))")
+    public void controllerPointcut() {
+    }
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) {
+
+        // 增加日志流水号 (格式：一毫秒内 + 随机数), 建议使用雪花算法
+        // LOG_ID 对应 logback-spring.xml 中的 %X{LOG_ID}, 用于输出日志时打印流水号, 方便排查问题
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // 打印请求信息
+        LOG.info("------------- 开始 -------------");
+        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
+        LOG.info("远程地址: {}", request.getRemoteAddr());
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+        // LOG.info("请求参数: {}", JSONObject.toJSONString(args));
+
+        // 排除特殊类型的参数，如文件类型
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+//        String[] excludeProperties = {"password", "oldPassword", "newPassword", "confirmPassword", "idCard", "phone", "email"};
+        String[] excludeProperties = {"mobile"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("请求参数: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        String[] excludeProperties = {"mobile"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+}
