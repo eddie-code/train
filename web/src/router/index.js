9411d7ebc9aa36dfbf42616a988c21b51dc5501c
eddie-code <231071372@qq.com>
2024-05-29 01:34:58 +0800

    5.12 增加控制台主页

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,31 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-    // 懒加载方式
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/LoginView.vue')
-  }
+    {
+        path: '/login',
+        component: () => import('../views/LoginView.vue')
+    },
+    {
+        path: '/',
+        component: () => import('../views/MainView.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
