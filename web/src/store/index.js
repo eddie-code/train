eeb8d9caaf992c9a19cafde1d8494f8c2fa56323
eddie-code <231071372@qq.com>
2024-05-30 12:14:47 +0800

    6.6 vuex配合h5 session缓存, 解决浏览器刷新丢失数据的问题

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,16 +1,22 @@
 import {createStore} from 'vuex'
 
+const MEMBER = "MEMBER";
+
 export default createStore({
     // 类似 Java 实体类字段 全局变量
     state: {
-        member: {}
+        // 优先获取左边缓存里面的数据, 如果缓存是空, 这样就可以保证 member 一定是一个对象.
+        // 使用 || {} 方式可以避免空指针异常, 因为 || 是短路求值, 如果左边是空, 就不会执行右边的代码.
+        member: window.SessionStorage.get(MEMBER) || {}
     },
     // 类似 Java get方法
     getters: {},
     // 类似 Java set方法
     mutations: {
         setMember(state, _member) {
-            state.member = _member
+            state.member = _member;
+            // 将数据同步到缓存
+            window.SessionStorage.set(MEMBER, _member);
         }
     },
     actions: {},
