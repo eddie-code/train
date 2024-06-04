1c3592cfa7825b2f5a690b098d5f9e67375a688a
eddie-code <231071372@qq.com>
2024-06-04 18:03:22 +0800

    7.3 增加乘车嗯新增接口, 增加字段非空校验

--- train-member/src/main/java/com/example/member/req/PassengerSaveReq.java
+++ train-member/src/main/java/com/example/member/req/PassengerSaveReq.java
@@ -1,5 +1,8 @@
 package com.example.member.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.ToString;
 
@@ -9,18 +12,45 @@ import java.util.Date;
 @ToString
 public class PassengerSaveReq {
 
+    /**
+     * id
+     */
     private Long id;
 
+    /**
+     * 会员id
+     */
+    @NotNull(message = "【会员ID】不能为空")
     private Long memberId;
 
+    /**
+     * 姓名
+     */
+    @NotBlank(message = "【姓名】不能为空")
     private String name;
 
+    /**
+     * 身份证
+     */
+    @NotBlank(message = "【身份证】不能为空")
     private String idCard;
 
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
+    /**
+     * 新增时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
 }
