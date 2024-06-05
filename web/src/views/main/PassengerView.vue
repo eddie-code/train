4c709ac0378f526bcc70a36fb86887f858bbdda4
eddie-code <231071372@qq.com>
2024-06-05 21:42:31 +0800

    7.11 乘车人列表查询界面开发, 引入表格组件

--- web/src/views/main/PassengerView.vue
+++ web/src/views/main/PassengerView.vue
@@ -1,22 +1,25 @@
 <template>
-    <a-button type="primary" @click="showModal">新增</a-button>
-    <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
-        <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
-            <a-form-item label="姓名">
-                <a-input v-model:value="passenger.name"/>
-            </a-form-item>
-            <a-form-item label="身份证">
-                <a-input v-model:value="passenger.idCard"/>
-            </a-form-item>
-            <a-form-item label="旅客类型">
-                <a-select v-model:value="passenger.type">
-                    <a-select-option value="1">成人</a-select-option>
-                    <a-select-option value="2">儿童</a-select-option>
-                    <a-select-option value="3">学生</a-select-option>
-                </a-select>
-            </a-form-item>
-        </a-form>
-    </a-modal>
+    <p>
+        <a-button type="primary" @click="showModal">新增</a-button>
+    </p>
+        <a-table :dataSource="dataSource" :columns="columns"/>
+        <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
+            <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+                <a-form-item label="姓名">
+                    <a-input v-model:value="passenger.name"/>
+                </a-form-item>
+                <a-form-item label="身份证">
+                    <a-input v-model:value="passenger.idCard"/>
+                </a-form-item>
+                <a-form-item label="旅客类型">
+                    <a-select v-model:value="passenger.type">
+                        <a-select-option value="1">成人</a-select-option>
+                        <a-select-option value="2">儿童</a-select-option>
+                        <a-select-option value="3">学生</a-select-option>
+                    </a-select>
+                </a-form-item>
+            </a-form>
+        </a-modal>
 </template>
 
 <script>
@@ -39,6 +42,36 @@ export default defineComponent({
             createTime: undefined,
             updateTime: undefined,
         });
+
+        const dataSource = [{
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+        }, {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+        }];
+
+        const columns = [
+            {
+                title: '姓名',
+                dataIndex: 'name',
+                key: 'name',
+            },
+            {
+                title: '年龄',
+                dataIndex: 'age',
+                key: 'age',
+            },
+            {
+                title: '住址',
+                dataIndex: 'address',
+                key: 'address',
+            }];
+
         const showModal = () => {
             visible.value = true;
         };
@@ -59,6 +92,8 @@ export default defineComponent({
             visible,
             showModal,
             handleOk,
+            dataSource,
+            columns
         };
     },
 });
