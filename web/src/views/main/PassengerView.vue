a7cb9d9169f4450679c4bba54159196641585c84
eddie-code <231071372@qq.com>
2024-06-06 19:41:32 +0800

    7.11 乘车人列表查询界面开发, 增加表格分页点击事件

--- web/src/views/main/PassengerView.vue
+++ web/src/views/main/PassengerView.vue
@@ -2,7 +2,11 @@
     <p>
         <a-button type="primary" @click="showModal">新增</a-button>
     </p>
-    <a-table :dataSource="passengers" :columns="columns" :pagination="pagination"/>
+    <a-table :dataSource="passengers"
+             :columns="columns"
+             :pagination="pagination"
+             @change="handleTableChange"
+    />
     <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
         <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
             <a-form-item label="姓名">
@@ -96,6 +100,8 @@ export default defineComponent({
                 let data = response.data;
                 if (data.success) {
                     passengers.value = data.content.list;
+                    // 设置分页控件的值
+                    pagination.current = param.page;
                     pagination.total = data.content.total;
                 } else {
                     notification.error({description: data.message});
@@ -103,10 +109,18 @@ export default defineComponent({
             });
         }
 
+        const handleTableChange = (pagination) => {
+            // console.log("看看自带的分页参数都有啥：" + pagination);
+            handleQuery({
+                page: pagination.current,
+                size: pagination.pageSize
+            });
+        };
+
         onMounted(() => {
             handleQuery({
                 page: 1,
-                size: 2
+                size: pagination.pageSize
             });
         });
 
@@ -117,7 +131,8 @@ export default defineComponent({
             handleOk,
             passengers,
             pagination,
-            columns
+            columns,
+            handleTableChange
         };
     },
 });
