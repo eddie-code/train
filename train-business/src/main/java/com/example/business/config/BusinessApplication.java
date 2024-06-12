32dc0b6adac0ab364111dce50482883fbeee46c6
eddie-code <231071372@qq.com>
2024-06-12 14:09:19 +0800

    9.4 项目中增加business业务模块

---
+++ train-business/src/main/java/com/example/business/config/BusinessApplication.java
@@ -0,0 +1,29 @@
+package com.example.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author lee
+ * @description
+ */
+@SpringBootApplication
+@ComponentScan("com.example")
+@MapperScan("com.example.business.mapper")
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
+
+}
