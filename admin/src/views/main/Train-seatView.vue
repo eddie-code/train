12dd01e3b71a7ee19365985712993728820f2486
eddie-code <231071372@qq.com>
2024-06-15 12:03:32 +0800

    9.17 修复火车座位页面的列号显示重覆的问题

--- admin/src/views/main/Train-seatView.vue
+++ admin/src/views/main/Train-seatView.vue
@@ -13,13 +13,13 @@
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
       </template>
-      <template v-else-if="column.dataIndex === 'col'">
+        <template v-else-if="column.dataIndex === 'col'">
         <span v-for="item in SEAT_COL_ARRAY" :key="item.code">
-          <span v-if="item.code === record.col">
+          <span v-if="item.code === record.col && item.type === record.seatType">
             {{item.desc}}
           </span>
         </span>
-      </template>
+        </template>
       <template v-else-if="column.dataIndex === 'seatType'">
         <span v-for="item in SEAT_TYPE_ARRAY" :key="item.code">
           <span v-if="item.code === record.seatType">
