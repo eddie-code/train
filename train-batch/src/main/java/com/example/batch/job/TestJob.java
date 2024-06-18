5fb1ef3ce93148b779dc8075f765e8b5a2ecdf1e
eddie-code <231071372@qq.com>
2024-06-18 20:15:46 +0800

    10.6 使用@DisallowConcurrentExecution, 禁用并发执行

--- train-batch/src/main/java/com/example/batch/job/TestJob.java
+++ train-batch/src/main/java/com/example/batch/job/TestJob.java
@@ -5,17 +5,17 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-@DisallowConcurrentExecution
+@DisallowConcurrentExecution // 禁用并发执行
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("TestJob TEST开始");
-        // try {
-        //     Thread.sleep(3000);
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        // }
-//        System.out.println("TestJob TEST结束");
+         try {
+             Thread.sleep(3000);
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+        System.out.println("TestJob TEST结束");
     }
 }
