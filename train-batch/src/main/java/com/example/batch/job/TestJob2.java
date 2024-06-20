50fdc017d9bb4dd26f552a6d25b37088f7b3554a
eddie-code <231071372@qq.com>
2024-06-20 13:07:46 +0800

    10.8 多节点场景下Quartz的调度情况测试 1. 复制多一个启动类, 通过IDEA修改端口 -Dserver.port=18003 2. 复制多个TestJob， 然后页面添加, 后台查看打印情况 3. 结论：（1）单一个服务启动, 只会在一个服务运行 (2) 多个服务情况下会在不同服务轮流执行

--- train-batch/src/main/java/com/example/batch/job/TestJob2.java
+++ train-batch/src/main/java/com/example/batch/job/TestJob2.java
@@ -6,16 +6,16 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 @DisallowConcurrentExecution // 禁用并发执行
-public class TestJob implements Job {
+public class TestJob2 implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST开始");
+        System.out.println("TestJob2 TEST开始");
 //         try {
 //             Thread.sleep(3000);
 //         } catch (InterruptedException e) {
 //             e.printStackTrace();
 //         }
-        System.out.println("TestJob TEST结束");
+        System.out.println("TestJob2 TEST结束");
     }
 }
