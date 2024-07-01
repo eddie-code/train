0b446d3b48cd86fedf3bf6671b45fa0ab9092615
eddie-code <231071372@qq.com>
2024-07-01 13:10:45 +0800

    12.8 订单页面, 优化购票列表的展示

--- web/src/views/main/OrderView.vue
+++ web/src/views/main/OrderView.vue
@@ -21,8 +21,35 @@
     <a-checkbox-group v-model:value="passengerChecks" :options="passengerOptions"/>
     <br/>
   选中的乘客：{{ passengerChecks }}
-  <br/>
+    <br/>
   购票列表：{{ tickets }}
+    <div class="order-tickets">
+        <a-row class="order-tickets-header" v-if="tickets.length > 0">
+            <a-col :span="2">乘客</a-col>
+            <a-col :span="6">身份证</a-col>
+            <a-col :span="4">票种</a-col>
+            <a-col :span="4">座位类型</a-col>
+        </a-row>
+        <a-row class="order-tickets-row" v-for="ticket in tickets" :key="ticket.passengerId">
+            <a-col :span="2">{{ ticket.passengerName }}</a-col>
+            <a-col :span="6">{{ ticket.passengerIdCard }}</a-col>
+            <a-col :span="4">
+                <a-select v-model:value="ticket.passengerType" style="width: 100%">
+                    <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code" :value="item.code">
+                        {{ item.desc }}
+                    </a-select-option>
+                </a-select>
+            </a-col>
+            <a-col :span="4">
+                <a-select v-model:value="ticket.seatTypeCode" style="width: 100%">
+                    <a-select-option v-for="item in seatTypes" :key="item.code" :value="item.code">
+                        {{ item.desc }}
+                    </a-select-option>
+                </a-select>
+            </a-col>
+        </a-row>
+    </div>
+
 </template>
 
 <script>
@@ -75,9 +102,10 @@ export default defineComponent({
         //   seat: "C1"
         // }
         const tickets = ref([]);
+        const PASSENGER_TYPE_ARRAY = window.PASSENGER_TYPE_ARRAY;
 
         // 勾选或去掉某个乘客时，在购票列表中加上或去掉一张表
-        watch(() => passengerChecks.value, (newVal, oldVal)=>{
+        watch(() => passengerChecks.value, (newVal, oldVal) => {
             console.log("勾选乘客发生变化", newVal, oldVal)
             // 每次有变化时，把购票列表清空，重新构造列表
             tickets.value = [];
@@ -117,6 +145,7 @@ export default defineComponent({
             passengerOptions,
             passengerChecks,
             tickets,
+            PASSENGER_TYPE_ARRAY,
         };
     },
 });
@@ -126,5 +155,25 @@ export default defineComponent({
 .order-train .order-train-main {
     font-size: 18px;
     font-weight: bold;
+}
+
+.order-tickets {
+    margin: 10px 0;
+}
+.order-tickets .ant-col {
+    padding: 5px 10px;
+}
+.order-tickets .order-tickets-header {
+    background-color: cornflowerblue;
+    border: solid 1px cornflowerblue;
+    color: white;
+    font-size: 16px;
+    padding: 5px 0;
+}
+.order-tickets .order-tickets-row {
+    border: solid 1px cornflowerblue;
+    border-top: none;
+    vertical-align: middle;
+    line-height: 30px;
 }
 </style>
