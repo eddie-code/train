0b00a1df2a58533a266934402741e93f7cea9d5d
eddie-code <231071372@qq.com>
2024-07-03 10:28:11 +0800

    12.10 chooseSeatObj先清空, 再初始化, 保证两排座位是有序的

--- web/src/views/main/OrderView.vue
+++ web/src/views/main/OrderView.vue
@@ -99,6 +99,7 @@
             </div>
             <br/>
             最终购票: {{tickets}}
+            最终选座: {{chooseSeatObj}}
         </div>
     </a-modal>
 </template>
@@ -184,6 +185,7 @@ export default defineComponent({
         // }
         const chooseSeatObj = ref({});
         watch(() => SEAT_COL_ARRAY.value, () => {
+            // 再次强调: 先清空, 再次赋值
             chooseSeatObj.value = {};
             for (let i = 1; i <= 2; i++) {
                 SEAT_COL_ARRAY.value.forEach((item) => {
