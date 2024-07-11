1b3a4262271d30e25ee5993af923a38db7d29440
eddie-code <231071372@qq.com>
2024-07-11 22:14:40 +0800

    13.4 集成Nacos配置中心, 实现动态刷新的功能

--- train-member/src/main/java/com/example/member/controller/TestController.java
+++ train-member/src/main/java/com/example/member/controller/TestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  */
 @RestController
-//@RefreshScope
+@RefreshScope
 public class TestController {
 
     @Value("${test.nacos}")
