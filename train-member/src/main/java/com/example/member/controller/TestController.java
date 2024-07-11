c7bb244f2d7517531cf7719c10e44642f36f679c
eddie-code <231071372@qq.com>
2024-07-11 14:41:16 +0800

    13.4 读取配置文件的配置

--- train-member/src/main/java/com/example/member/controller/TestController.java
+++ train-member/src/main/java/com/example/member/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.example.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  */
 @RestController
+//@RefreshScope
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
+//    @Autowired
+//    Environment environment;
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello world!";
+        return String.format("Hello %s!", testNacos);
+//        String port = environment.getProperty("local.server.port");
+//        return String.format("Hello %s! 端口：%s", testNacos, port);
     }
 
 }
