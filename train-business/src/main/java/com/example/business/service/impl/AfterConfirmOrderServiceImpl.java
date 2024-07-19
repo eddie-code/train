bfc55868c106a43d2414a6ca63f5c3fc3225d887
eddie-code <231071372@qq.com>
2024-07-19 11:15:22 +0800

    15.7 配置seata+nacos, 分布式事务测试成功, Nacos配置里面需要放开 service.vgroupMapping.xxxx 开头的配置

--- train-business/src/main/java/com/example/business/service/impl/AfterConfirmOrderServiceImpl.java
+++ train-business/src/main/java/com/example/business/service/impl/AfterConfirmOrderServiceImpl.java
@@ -145,7 +145,7 @@ public class AfterConfirmOrderServiceImpl implements AfterConfirmOrderService {
                 confirmOrderMapper.update(null, uw);
 
                 // 模拟调用方出现异常
-                 Thread.sleep(10000);
+//                 Thread.sleep(10000);
                  if (1 == 1) {
                      throw new Exception("测试异常");
                  }
