e70b8ca58b39661132023e99af054394426a3d20
eddie-code <231071372@qq.com>
2024-07-25 21:18:24 +0800

    17.6 使用Sentinel配置限流 1. 先使用jmeter请求接口 2. 在控制台-簇点链路-添加流控 3. 再次使用jmeter请求, 查看结果树, 控制台查看实时监控

--- train-business/src/main/java/com/example/business/controller/web/ConfirmOrderController.java
+++ train-business/src/main/java/com/example/business/controller/web/ConfirmOrderController.java
@@ -1,15 +1,21 @@
 package com.example.business.controller.web;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.business.req.ConfirmOrderDoReq;
 import com.example.business.service.ConfirmOrderService;
+import com.example.common.exception.BusinessException;
+import com.example.common.exception.BusinessExceptionEnum;
 import com.example.common.resp.CommonResp;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/confirm-order")
 public class ConfirmOrderController {
@@ -17,10 +23,27 @@ public class ConfirmOrderController {
     @Autowired
     private ConfirmOrderService confirmOrderService;
 
+    /**
+     * 接口的资源名称不要和接口路径一致，否则会导致限流后走不到降级方法中
+     */
+    @SentinelResource(value = "confirmOrderDo", blockHandler = "doConfirmBlock")
     @PostMapping("/do")
     public CommonResp<Object> doConfirm(@Valid @RequestBody ConfirmOrderDoReq req) {
         confirmOrderService.doConfirm(req);
         return new CommonResp<>();
+    }
+
+    /**
+     * 降级方法，需包含限流方法的所有参数和BlockException参数, 并且返回值类型必须和限流方法一致
+     */
+    public CommonResp<Object> doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+        log.info("ConfirmOrderController.doConfirm.doConfirmBlock.购票请求被限流：{}", req);
+//        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
+        CommonResp<Object> commonResp = new CommonResp<>();
+        commonResp.setSuccess(false);
+        // 当前抢票人数太多了，请稍候重试
+        commonResp.setMessage(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION.getDesc());
+        return commonResp;
     }
 
 }
